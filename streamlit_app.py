29934171638e0956cea24a994471c0f83d0c96b9
puneet <mysfacc2@gmail.com>
2023-01-02 22:36:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ streamlit.header('hello my full name is puneet K')
 streamlit.text('hello my  location is bangalore')
 
 streamlit.text('test app1')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
