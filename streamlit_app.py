8bcf9bdb3cbe19d8dd5eea7045bc75ad5970adb0
puneet <mysfacc2@gmail.com>
2023-01-02 22:43:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
 import streamlit
-streamlit.title('hello my name is puneet')
-
-
-streamlit.header('hello my full name is puneet K')
-
-streamlit.text('hello my  location is bangalore')
-
-streamlit.text('test app1')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
