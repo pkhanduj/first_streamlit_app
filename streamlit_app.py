a68e0878d9627bb09f7b0b92db26b5db476967aa
puneet <mysfacc2@gmail.com>
2023-01-07 23:02:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,6 +80,17 @@ streamlit.stop()
 #######################################streamlit.text("Hello from Snowflake:")
 ######################################################streamlit.text(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('from streamlit')")
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
+#my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('from streamlit')")
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('from streamlit')")
+    return(" thanks for adding "+ new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+if streamlit.button('Add a fruit to the list'):
+           my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+           back_from_function =insert_row_snowflake(add_my_fruit)
+           streamlit.text(back_from_function)
