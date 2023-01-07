2ce53d2da0973158f6f048ca1a88b489fe8b1a23
puneet <mysfacc2@gmail.com>
2023-01-07 22:09:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-##streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#####################################################streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
@@ -22,14 +22,16 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+###########################################fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 ##streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+############################################### write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+############################################ write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
 streamlit.stop()
 
 
@@ -39,10 +41,10 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row=my_cur.fetchall()
 streamlit.header("the fruit loadlist contains:")
 streamlit.dataframe(my_data_row)
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+#####################################my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+######################################my_data_row = my_cur.fetchone()
+#######################################streamlit.text("Hello from Snowflake:")
+######################################################streamlit.text(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
