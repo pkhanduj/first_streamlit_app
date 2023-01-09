9189aa188d3101ab2e60ef691c0bfe424fc0332f
puneet <mysfacc2@gmail.com>
2023-01-09 19:04:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title('PUNEET BREAKFAST KITCHEN AND SMOOTHIE')
+streamlit.title('PUNEET BREAKFAST KITCHEN&SMOOTHIE')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
